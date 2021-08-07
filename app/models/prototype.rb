39d5566c9fcf47011c548567de86c7995d4166e9
Taiki Noda <taiki.nd.real@gmail.com>
2021-08-07 21:28:59 +0900

    prototypeのバリデーションを設定

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,7 +1,12 @@
 class Prototype < ApplicationRecord
 
+  validates :title, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true
+  validates :image, presence: true
+
   has_one_attached :image
-  
+
   belongs_to :user
   
 end
