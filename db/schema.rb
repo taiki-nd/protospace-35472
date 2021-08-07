15951241a2029f7e116d489579562b8d97eaaa0a
Taiki Noda <taiki.nd.real@gmail.com>
2021-08-07 21:11:14 +0900

    Prototypeモデルおよびテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_07_072654) do
+ActiveRecord::Schema.define(version: 2021_08_07_115229) do
+
+  create_table "prototypes", charset: "utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "catch_copy"
+    t.text "concept"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_prototypes_on_user_id"
+  end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
