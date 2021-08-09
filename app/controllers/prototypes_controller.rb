2f013a5a05c62c1892927d1e6cdd7a61b46b8836
Taiki Noda <taiki.nd.real@gmail.com>
2021-08-09 22:06:57 +0900

    ログインユーザーへの規制

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -28,6 +28,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
   end
 
   def update
@@ -51,8 +54,7 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    @prototype = Prototype.find(params[:id])
-    unless user_signed_in? && current_user.id == @prototype.user_id
+    unless user_signed_in?
       redirect_to action: :index
     end
   end 
